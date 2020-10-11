58f4013b7b4e37839e3df134026df3a26d1105ec
crazed014 <brandon.childers@juabsd.org>
2020-10-11 18:44:32 +0000

    Added script to setup dev environment

--- combinate.py
+++ combinate.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 from flask_dropzone import Dropzone
-
+import os
 
 app = Flask(__name__)
 dropzone = Dropzone(app)
 viewcount = 0
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app.config.update(
+    UPLOADED_PATH=os.path.join(basedir, 'uploads'),
+    # Flask-Dropzone config:
+    DROPZONE_MAX_FILES=3,
+    DROPZONE_UPLOAD_ON_CLICK=True
+)
 @app.route("/")
 def welcome():
     return render_template("welcome.html", title="Combinate", formtext="Drag your first CSV here.")
@@ -16,4 +24,12 @@ def viewed():
     viewcount += 1
     return "This page has been served {} times since the last reboot.".format(str(viewcount))
 
+@app.route('/uploads', methods=['GET', 'POST'])
+def upload():
+
+    if request.method == 'POST':
+        f = request.files.get('file')
+        f.save(os.path.join('/srv/combinate/files/', f.filename))
+
+    return render_template("welcome.html", title="Combinate", formtext="Drag your second CSV here.")
 #  flask run --host=0.0.0.0
---
+++ setupenv.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+apt update
+apt upgrade
+apt install python3-pip
