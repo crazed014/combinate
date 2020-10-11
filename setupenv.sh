82320613c88d630c5f68c27a61cd5c3c9e031c83
root <brandon.childers@juabsd.org>
2020-10-11 19:51:18 +0000

    created runner

--- setupenv.sh
+++ setupenv.sh
@@ -3,8 +3,6 @@ apt update
 apt upgrade -y
 apt install -y python3-pip python3-flask
 pip3 install flask-dropzone
-cd combinate
-source bin/activate
-export FLASK_APP=combinate.py
-export FLASK_ENV=development
+
+
 
